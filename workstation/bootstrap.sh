6da7d3bfb7bddae5b5516a95caf72eb51d855cec
Max Schoening <max@max.wtf>
2019-07-15 13:26:56 -0700

    Update repo to clone from GitHub on bootstrap

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -309,10 +309,10 @@ if [ ! -d /mnt/dev/code/dotfiles ]; then
   # the reason we dont't copy the files individually is, to easily push changes
   # if needed
   cd "/mnt/dev/code"
-  git clone --recursive https://github.com/fatih/dotfiles.git
+  git clone --recursive https://github.com/max/dotfiles.git
 
   cd "/mnt/dev/code/dotfiles"
-  git remote set-url origin git@github.com:fatih/dotfiles.git
+  git remote set-url origin git@github.com:max/dotfiles.git
 
   ln -sfn $(pwd)/vimrc "${HOME}/.vimrc"
   ln -sfn $(pwd)/zshrc "${HOME}/.zshrc"
